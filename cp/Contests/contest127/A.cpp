4f191f5bc7cc653339451ef55d6db4ba37b9f9b4
VarunBhojane <varunbhojane07@gmail.com>
2024-03-30 12:27:00 +0530

    contest 127 's my solutions

---
+++ cp/Contests/contest127/A.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+#define int long long int
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl
+#define MOD 1000000007
+//const int maxsize = 1e5;
+//int arr[maxsize+1];
+#define test int monu;cin>>monu;while(monu--)
+using namespace std;
+signed main()
+{
+test
+{
+    int n,k,x;
+    cin>>n>>k>>x;
+    while(x>0 and k)
+    {
+       x = x-1;
+       x = (x+1)/2;
+       k--;
+    }
+    if(k==0)
+    {
+        yes;
+    }
+    else{
+        no;
+    }
+}
+//dusro ka code copy karne se ghar nahi chalta bhai
+}
---
+++ cp/Contests/contest127/B.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#define int long long int
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl
+#define MOD 1000000007
+//const int maxsize = 1e5;
+//int arr[maxsize+1];
+#define test int monu;cin>>monu;while(monu--)
+using namespace std;
+signed main()
+{
+test
+{
+    int n,k;cin>>n>>k;
+    if(1e9-k>n)
+    {
+        for(int i =0)
+    }
+}
+//dusro ka code copy karne se ghar nahi chalta bhai
+}
